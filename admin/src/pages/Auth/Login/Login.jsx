81c6698b6de643084ebfc412776f49991f040075
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-04-30 17:36:57 +0530

    changes in login page

--- admin/src/pages/Auth/Login/Login.jsx
+++ admin/src/pages/Auth/Login/Login.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { FaRegEye } from "react-icons/fa";
 import griffinLogo from "../../../assets/images/griffinLogo.png";
+import loginImage from "../../../assets/images/login2.jpg";
 // ----------------------------------------------------------------------------------
 const Login = () => {
   const [toggle, setToggle] = useState(false); // This usestate is used to toggle the password show and hide icons and input type.
@@ -30,164 +31,75 @@ const Login = () => {
   // }, [isLoginOtpGenerated]);
 
   return (
-    <div className="container mx-auto h-[80vh] grid place-items-center px-4">
-      <div className="rounded  bg-white shadow-default dark:border-strokedark dark:bg-boxdark grid place-items-center login-container   ">
-        <div className="flex flex-wrap items-center justify-center">
-          <div className="hidden w-full xl:block xl:w-1/2">
-            <div className="py-17.5 px-26 text-center">
-              <Link className="mb-5.5 inline-block" to="/">
-                <img
-                  className="hidden dark:block"
-                  src={griffinLogo}
-                  alt="Logo"
-                  width={400}
-                  height={400}
+    <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="flex justify-center">
+            <img src={griffinLogo} class="w-mx-auto" />
+          </div>
+          <div class=" flex flex-col items-center">
+            <div class="w-full flex-1 mt-8">
+              <div class="mx-auto max-w-xs">
+                <input
+                  class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  type="email"
+                  placeholder="Email"
                 />
-                <img
-                  className="dark:hidden"
-                  src={griffinLogo}
-                  alt="Logo"
-                  width={400}
-                  height={400}
+                <input
+                  class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="password"
+                  placeholder="Password"
                 />
-              </Link>
+                <button
+                  class="mt-5 tracking-wide font-semibold bg-green-400 text-white-500 w-full py-4 rounded-lg text-white hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  style={{
+                    background:
+                      "linear-gradient(to right, rgb(238, 119, 36), rgb(178 11 11), rgb(182 21 21), rgb(198 72 72))",
+                  }}
+                >
+                  <svg
+                    class="w-6 h-6 -ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span class="text-white ml-2"> Sign In</span>
+                </button>
+                {/* <p class="mt-6 text-xs text-gray-600 text-center">
+                  I agree to abide by Cartesian Kinetics
+                  <a href="#" class="border-b border-gray-500 border-dotted">
+                    Terms of Service
+                  </a>
+                  and its
+                  <a href="#" class="border-b border-gray-500 border-dotted">
+                    Privacy Policy
+                  </a>
+                </p> */}
+              </div>
             </div>
           </div>
-
-          <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
-            <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-              <h2 className="mb-9 text-2xl font-bold text-[darkblue] dark:text-white sm:text-title-xl2 text-center">
-                Sign in to C.RM Admin Panel
-              </h2>
-
-              <form onSubmit={() => {}}>
-                <div className="mb-4">
-                  <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Enter Email Address
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="email"
-                      placeholder="Please Enter your email id"
-                      className="w-full rounded-lg border border-stroke bg-white text-black py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                      // {...register('email', {
-                      //   required: {
-                      //     value: true,
-                      //     message: 'Email Address is required',
-                      //   },
-                      //   pattern: {
-                      //     value: emailRegex,
-                      //     message: 'Email Address is invalid',
-                      //   },
-                      // })}
-                      id="email"
-                    />
-                    {/* {errors?.email && (
-                      <div className="text-danger pt-1">
-                        {errors?.email?.message || "Email is required"}
-                      </div>
-                    )} */}
-                    <span className="absolute right-4 top-4">
-                      <svg
-                        className="fill-current"
-                        width="22"
-                        height="22"
-                        viewBox="0 0 22 22"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g opacity="0.5">
-                          <path
-                            d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                            fill=""
-                          />
-                        </g>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-
-                <div className="mb-6">
-                  <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Enter Password
-                  </label>
-                  <div className="relative">
-                    <input
-                      type={`${toggle ? "text" : "password"}`}
-                      autoComplete="off"
-                      placeholder="Please Enter your Password"
-                      className="w-full rounded-lg border bg-white text-black border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                      // {...register('password', {
-                      //   required: {
-                      //     value: true,
-                      //     message: 'Login Password is required',
-                      //   },
-                      //   pattern: {
-                      //     value: passwordRegex,
-                      //     message:
-                      //       'Login Password is invalid. Password should contain minimum 1 upper case character, 1 lower case character, 1 special character and 1 number with minimum length of 10 and maximum length of 20.',
-                      //   },
-                      // })}
-                    />
-                    {/* {errors?.password && (
-                      <div className="text-danger pt-1">
-                        {errors?.password?.message ||
-                          "Login Password is required"}
-                      </div>
-                    )} */}
-
-                    <span
-                      className="absolute right-4 top-4"
-                      onClick={() => {
-                        setToggle(!toggle);
-                      }}
-                    >
-                      {toggle ? (
-                        <FaRegEye
-                          fontSize={22}
-                          title="Click to Hide the Login Password"
-                        />
-                      ) : (
-                        <FaRegEyeSlash
-                          fontSize={22}
-                          title="Click to View the Login Password"
-                        />
-                      )}
-                    </span>
-                  </div>
-                </div>
-
-                <div className="mb-5">
-                  <button
-                    type="submit"
-                    className="flex justify-center items-center w-full cursor-pointer rounded-lg border border-meta-5 bg-meta-5 p-4 text-white transition hover:bg-opacity-90"
-                  >
-                    Login
-                    {false && Object.keys(loginData).length > 0 && (
-                      <div
-                        className="inline-block ml-2 h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                        role="status"
-                      >
-                        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                          Loading...
-                        </span>
-                      </div>
-                    )}
-                  </button>
-                </div>
-                <div>
-                  <p className="text-[black] text-right">
-                    Are you Forgot your Password? &nbsp;
-                    <span
-                      className="text-[blue] font-bold underline cursor-pointer "
-                      onClick={() => navigate("/auth/resetPassword")}
-                    >
-                      Reset Password
-                    </span>
-                  </p>
-                </div>
-              </form>
-            </div>
+        </div>
+        <div
+          class="flex-1 bg-green-100 text-center hidden lg:flex"
+          style={{
+            background:
+              "linear-gradient(to right, rgb(238, 119, 36), rgb(178 11 11), rgb(182 21 21), rgb(198 72 72))",
+          }}
+        >
+          <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat flex flex-col justify-center items-center">
+            <h1 className="text-4xl font-bold text-white">
+              Welcome to Griffin digital
+            </h1>
+            <p className="text-md text-white">
+              We started with a dream to redefine creativity and have since
+              embarked on a journey marked by unwavering passion and innovation.
+            </p>
           </div>
         </div>
       </div>
