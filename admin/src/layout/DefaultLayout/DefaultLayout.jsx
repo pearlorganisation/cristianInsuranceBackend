a4acb9f0d84ba12cd85ad49e39522550e88c87cf
Saksham <saksham@pearlorganisation.com>
2024-04-30 15:42:03 +0530

    added the layout

---
+++ admin/src/layout/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+
+const DefaultLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default DefaultLayout;
